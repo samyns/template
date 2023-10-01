64c751bc4efa90983315a2860b1d228e4b93869c
samyns <21samuel.gerard@gmail.com>
2023-10-01 19:54:57 +0200

    template pour react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './Styles/App.css';
+import './Styles/reset.css';
+
+// src/App.js
+
+import React from 'react';
+import Titre from './Components/Titre';
+
+function App() {
+    return (
+        <div>
+            <p>Tadam !</p>
+
+            <Titre id="un" />
+        </div>
+    );
+}
+
+export default App;
+
---
+++ src/Components/Titre.js
@@ -0,0 +1,9 @@
+// src/components/MonComposant.js
+
+import React from 'react';
+
+const MonComposant = ({ id }) => {
+    return <p>Bonjour Samuel !</p>;
+};
+
+export default MonComposant;
